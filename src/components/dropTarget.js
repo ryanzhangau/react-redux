8c908cc54bff3f4d5fbc39b2f55cc76ef892ec2c
RyanZhang <ryanzhangau@gmail.com>
2017-10-05 22:44:49 +1100

    drag enter and leave state change

--- src/components/dropTarget.js
+++ src/components/dropTarget.js
@@ -1,24 +1,28 @@
 import React from 'react';
 
 export default class DropTarget extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { dragEnter: false };
+  }
 
   getClass () {
-    return (this.props.blocks.length > 0 ? 'drop-target' : 'init');
+    return `${this.props.blocks.length > 0 ? 'drop-target' : 'init'}  ${this.state.dragEnter ? 'active' : ''}`;
   }
 
   dragEnger() {
-    console.log('drag enter');
+    this.setState({ dragEnter: true });
   }
 
   dragLeave() {
-    console.log('drag leave');
+    this.setState({ dragEnter: false });
   }
 
   render () {
     return (
       <div className={this.getClass()}
-           onDragEnter={this.dragEnger}
-           onDragLeave={this.dragLeave}
+           onDragEnter={this.dragEnger.bind(this)}
+           onDragLeave={this.dragLeave.bind(this)}
       >
         {this.props.blocks.length > 0 ? '' : 'Drop Here'}
       </div>
