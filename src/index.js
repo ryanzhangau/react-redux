99400aa41e5dd4270b1fe8c297c7f6a741e4ba88
RyanZhang <ryanzhangau@gmail.com>
2017-10-05 23:19:00 +1100

    connect store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './containers/App';
+import { Provider } from 'react-redux';
+import store from './store';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
 registerServiceWorker();
