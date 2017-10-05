f4508db64d2ca44c5cd21707bf8e25a59b4ef85f
RyanZhang <ryanzhangau@gmail.com>
2017-10-05 22:16:00 +1100

    drag and drop

--- src/components/dropTarget.js
+++ src/components/dropTarget.js
@@ -6,9 +6,20 @@ export default class DropTarget extends React.Component {
     return (this.props.blocks.length > 0 ? 'drop-target' : 'init');
   }
 
+  dragEnger() {
+    console.log('drag enter');
+  }
+
+  dragLeave() {
+    console.log('drag leave');
+  }
+
   render () {
     return (
-      <div className={this.getClass()}>
+      <div className={this.getClass()}
+           onDragEnter={this.dragEnger}
+           onDragLeave={this.dragLeave}
+      >
         {this.props.blocks.length > 0 ? '' : 'Drop Here'}
       </div>
     )
--- src/components/modules.js
+++ src/components/modules.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
 export default class Module extends React.Component {
+
+  dragstart() {
+    console.log('drag start');
+  }
+
+  dragend() {
+    console.log('drag end');
+  }
   render() {
     return (
-      <div className={`module ${this.props.module.name}`}>
+      <div className={`module ${this.props.module.name}`} draggable="true"
+           onDragStart={this.dragstart}
+           onDragEnd={this.dragend}
+      >
         <i className={`fa fa-fw fa-${this.props.module.icon}`}></i>
         <p>{this.props.module.name}</p>
       </div>
