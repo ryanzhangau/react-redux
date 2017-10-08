024938d48c26a2ead4fbe54e3e591f4e1c0cb3fe
RyanZhang <ryanzhangau@gmail.com>
2017-10-08 19:37:15 +1100

    set transferData once drag start

--- src/components/modules.js
+++ src/components/modules.js
@@ -1,23 +1,39 @@
 import React from 'react';
+import { headingData, headingBlock } from "./blocks/Heading";
+import { textData, textBlock } from "./blocks/Text";
+import { imageData, imageBlock } from "./blocks/Image";
 
 export default class Module extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      modules: [
+        { block: headingBlock, data: headingData },
+        { block:textBlock, data: textData },
+        { block: imageBlock, data: imageData },
+      ]
+    }
+  }
 
-  dragstart() {
-    console.log('drag start');
+  dragstart(e, data) {
+    e.dataTransfer.setData('opts', JSON.stringify(data));
   }
 
   dragend() {
     console.log('drag end');
   }
   render() {
+    const blocks = this.state.modules.map((b) => {
+       return  <div className={`module ${b.block.name}`} key={b.block.order} draggable="true"
+             onDragStart={(e) => this.dragstart(e, b.data)}
+             onDragEnd={this.dragend}
+        >
+          <i className={`fa fa-fw fa-${b.block.icon}`}></i>
+          <p>{b.block.name}</p>
+        </div>;
+      })
     return (
-      <div className={`module ${this.props.module.name}`} draggable="true"
-           onDragStart={this.dragstart}
-           onDragEnd={this.dragend}
-      >
-        <i className={`fa fa-fw fa-${this.props.module.icon}`}></i>
-        <p>{this.props.module.name}</p>
-      </div>
+      <div>{ blocks }</div>
     );
   }
 }
