ad697f0fce5f94299f985087c83e6fb56f245eea
RyanZhang <ryanzhangau@gmail.com>
2017-10-08 19:58:56 +1100

    add logger for debug purpose

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import blockReducer from './reducers/blockReducer';
+import logger from 'redux-logger';
 
 export default createStore(combineReducers({
   blockReducer,
-}));
+}),applyMiddleware(logger));
